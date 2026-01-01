dec0c056b7e1caeaadb4e552122c715a762470ac
goldshoot0720 <goldshoot0720@gmail.com>
2026-01-01 17:01:13 +0800

    初始提交：鋒兄AI資訊系統 - WPF應用程序

---
+++ wpfkiro20260101/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfkiro20260101
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            NavigateToHome(null, null); // 默認顯示首頁
+        }
+
+        private void NavigateToHome(object? sender, RoutedEventArgs? e)
+        {
+            MainFrame.Navigate(new HomePage());
+        }
+
+        private void NavigateToDashboard(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new DashboardPage());
+        }
+
+        private void NavigateToPhotos(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new PhotosPage());
+        }
+
+        private void NavigateToVideos(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new VideosPage());
+        }
+
+        private void NavigateToSubscription(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new SubscriptionPage());
+        }
+
+        private void NavigateToFood(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new FoodPage());
+        }
+    }
+}
